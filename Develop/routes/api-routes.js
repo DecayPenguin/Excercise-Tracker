61d8539c8d8a5247be277521acd102d7eca78334
Decay <devinamccomb@gmail.com>
2020-12-07 16:25:30 -0600

    Declared router and Workout in api-route.js file

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,5 +1,37 @@
-const Workout = require("../models/workout/");
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
-module.exports = function(app){
-    app.get
-}
+
+router.post("/api/workout.js", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workout/:id", ({body, params}, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { excercises: body }},
+        {new: true, runValidators: true}
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(dbWorkout);
+    })
+});
+router.get("/api/workout/range", (req, res) => {
+    Workout.find()
+    .then(dbWorkouts);
+})
+.catch(err => {
+    res.json(dbWorkouts);
+})
+.catch(err => {
+    res.json(err);
+});
