6f089a21c258b72274591fb2737d4719c4777c10
Decay <devinamccomb@gmail.com>
2020-12-08 00:03:41 -0600

    Worked with tutor, added env file and operated via localhost

---
+++ app.js
@@ -0,0 +1,27 @@
+let express = require("express");
+let mongoose = require("mongoose");
+let morgan = require("morgan");
+let app = express()
+
+const PORT = process.env.PORT || 3030;
+
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("./Develop/public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./Develop/route/api-route.js"));
+app.use(require("./Develop/route/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
